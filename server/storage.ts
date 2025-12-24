66c13bb5e365bee6d67ddb7d199db7f8a515c58d
Digitalstarter <Info@digitalstarter.nl>
2025-12-24 04:58:01 +0100

    Update storage.ts

--- server/storage.ts
+++ server/storage.ts
@@ -1,4 +1,9 @@
-import { type ContactSubmission, type InsertContactSubmission, contactSubmissions } from "@shared/schema";
+import {
+  type ContactSubmission,
+  type InsertContactSubmission,
+  contactSubmissions
+} from "../schema";
+
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
@@ -10,16 +15,22 @@ const sql = neon(process.env.DATABASE_URL);
 const db = drizzle(sql);
 
 export interface IStorage {
-  createContactSubmission(submission: InsertContactSubmission): Promise<ContactSubmission>;
+  createContactSubmission(
+    submission: InsertContactSubmission
+  ): Promise<ContactSubmission>;
+
   getContactSubmissions(): Promise<ContactSubmission[]>;
 }
 
 export class PostgresStorage implements IStorage {
-  async createContactSubmission(insertSubmission: InsertContactSubmission): Promise<ContactSubmission> {
+  async createContactSubmission(
+    insertSubmission: InsertContactSubmission
+  ): Promise<ContactSubmission> {
     const [submission] = await db
       .insert(contactSubmissions)
       .values(insertSubmission)
       .returning();
+
     return submission;
   }
 
